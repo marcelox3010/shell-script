91212942926c665d73a83d461a67c3bdaeb2cf82
Marcelo Gadelha <marcelox3010@gmail.com>
2019-10-18 16:48:24 -0300

    Capturar comando usando crase.

--- variavel.sh
+++ variavel.sh
@@ -6,3 +6,6 @@ echo "3. I am ${MY_SHELL}ing on my keyboard"
 
 SERVER_NAME=$(hostname)
 echo "4. You are running this script on $SERVER_NAME"
+
+SERVER_NAME_2=`hostname`
+echo "5. You are running this script on $SERVER_NAME_2"
