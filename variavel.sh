ad2c657f6e76a3c15ea3d0ae663d6d37a02dc3f8
Marcelo Gadelha <marcelox3010@gmail.com>
2019-10-18 16:42:41 -0300

    Capturando um comando e colocando em uma variável

--- variavel.sh
+++ variavel.sh
@@ -3,3 +3,6 @@ MY_SHELL="bash"
 echo "1. I like the $MY_SHELL shell"
 echo "2. I like the ${MY_SHELL} shell"
 echo "3. I am ${MY_SHELL}ing on my keyboard"
+
+SERVER_NAME=$(hostname)
+echo "4. You are running this script on $SERVER_NAME"
