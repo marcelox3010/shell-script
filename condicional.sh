16d4bfadbe94093f352dd5becc5e14c9767c3aba
root <root@DESKTOP-QD32IIR.localdomain>
2019-10-18 23:03:01 -0300

    Exemplo de condicional.

---
+++ condicional.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+
+if [ "foo" = "foo" ]; then
+
+   echo "condicao 1 - verdadeira"
+
+fi
+
+if [ "foo 2" = "foo" ]; then
+
+	echo "condicao 2 - verdadeiro"
+
+else
+
+	echo "condicao 2 - falso"
+
+fi
