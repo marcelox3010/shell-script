dc274064eb08237d0050d66004f52fa1eddec21e
Marcelo Gadelha <marcelox3010@gmail.com>
2019-11-08 19:30:35 -0300

    exemplo de comando alinhado sem checagem.

--- comandos-dependentes.sh
+++ comandos-dependentes.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#Executa o segundo comando se o primeiro comando tiver sucesso
+# Executa o segundo comando se o primeiro comando tiver sucesso
 mkdir ~/myshell && touch ~/myshell/myfile.txt
 
-#executa o segundo comando se o primeiro comando falhar
+# Executa o segundo comando se o primeiro comando falhar
 mkdir ~/myshell || touch ~/myshell/error.txt
+
+# Executa dois comandos na mesma linha sem checagem de status
+ echo "oi mundo do shell-script" >> ~/myshell/myfile.txt; cat ~/myshell/myfile.txt
