4f05d9dc6d21880fe41cfe2537bf479bb2dbe88f
Marcelo Gadelha <marcelox3010@gmail.com>
2019-11-08 19:21:41 -0300

    exemplo de comando com ||

--- comandos-dependentes.sh
+++ comandos-dependentes.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#Executa o segundo comando se o primeiro comando tiver sucesso
 mkdir ~/myshell && touch ~/myshell/myfile.txt
+
+#executa o segundo comando se o primeiro comando falhar
+mkdir ~/myshell || touch ~/myshell/error.txt
